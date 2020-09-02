bfdd8e7d0ea0ff6b164aaf87cbc05db0dc416d65
Brord van Wierst <brord@iota.org>
2020-09-02 23:23:54 +0200

    Cheeky conversion

--- c/src/models.rs
+++ c/src/models.rs
@@ -19,6 +19,12 @@ use iota_streams::app_channels::api::tangle::{
     Transport as Trans,
 };
 
+use iota::{
+    ternary as iota_ternary,
+};
+use core::convert::{
+    TryInto,
+};
 
 use iota_streams::core_keccak::sponge::prp::keccak::KeccakF1600;
 use iota_streams::core_edsig::key_exchange::x25519;
@@ -33,7 +39,7 @@ pub extern "C" fn get_appinst_str(appinst: *mut AppInst) -> *mut c_char {
     unsafe {
         let unboxed = Box::from_raw(appinst).0;
         assert!(!unboxed.tbits().is_empty(), "App instance can't be empty");
-        CString::new(hex::encode(unboxed.tbits().clone())).unwrap().into_raw()
+        CString::from_vec_unchecked(bits_to_trytes(unboxed.tbits().clone())).into_raw()
     }
 }
 
@@ -44,7 +50,7 @@ pub extern "C" fn get_msgid_str(msgid: *mut MsgId) -> *mut c_char{
     unsafe {
         let unboxed = Box::from_raw(msgid).0;
         assert!(!unboxed.tbits().is_empty(), "Msg Id can't be empty");
-        CString::new(hex::encode(unboxed.tbits().clone())).unwrap().into_raw()    }
+        CString::from_vec_unchecked(bits_to_trytes(unboxed.tbits().clone())).into_raw()    }
 }
 
 pub struct Address(pub(crate) TangleAddress);
@@ -56,7 +62,7 @@ pub extern "C" fn get_address_inst_str(address: *mut Address) -> *mut c_char {
         assert!(!unboxed.0.appinst.tbits().is_empty(), "App instance can't be empty");
         self::get_appinst_str(Box::into_raw(Box::new(AppInst(unboxed.0.appinst.clone()))));
 
-        CString::new(hex::encode(unboxed.0.appinst.tbits().clone())).unwrap().into_raw()
+        CString::from_vec_unchecked(bits_to_trytes(unboxed.0.msgid.tbits().clone())).into_raw()
     }
 }
 
@@ -67,8 +73,28 @@ pub extern "C" fn get_address_id_str(address: *mut Address) -> *mut c_char {
         assert!(!unboxed.0.msgid.tbits().is_empty(), "App instance can't be empty");
         self::get_msgid_str(Box::into_raw(Box::new(MsgId(unboxed.0.msgid.clone()))));
 
-        CString::new(hex::encode(unboxed.0.msgid.tbits().clone())).unwrap().into_raw()
+        CString::from_vec_unchecked(bits_to_trytes(unboxed.0.msgid.tbits().clone())).into_raw()
     }
+}
+
+fn bits_to_trytes(input: Vec<u8>) -> Vec<u8> {
+    let mut trytes: std::vec::Vec<u8> = Vec::with_capacity(input.len() * 2);
+    for byte in input {
+        let first: i8 = match (byte % 27) as i8 {
+            b @ 0..=13 => b,
+            b @ 14..=26 => b - 27,
+            _ => unreachable!(),
+        };
+        let second = match (byte / 27) as i8 {
+            b @ 0..=13 => b,
+            b @ 14..=26 => b - 27,
+            _ => unreachable!(),
+        };
+
+        trytes.push(char::from(TryInto::<iota_ternary::Tryte>::try_into(first).unwrap()) as u8);
+        trytes.push(char::from(TryInto::<iota_ternary::Tryte>::try_into(second).unwrap()) as u8);
+    }
+    trytes
 }
 
 pub struct Transport<'a>(pub(crate) &'a iota::Client);
