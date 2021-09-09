4d294d73f560618aec0147f600036f056f07e8cd
Arnau Orriols <arnau.orriols@iota.org>
2021-09-09 15:16:41 +0200

    fmt of `channels::api::tagle::test`

--- iota-streams-app-channels/src/api/tangle/test.rs
+++ iota-streams-app-channels/src/api/tangle/test.rs
@@ -143,7 +143,10 @@ pub async fn example<T: Transport + Clone>(transport: T) -> Result<()> {
 
 #[test]
 fn run_basic_scenario() {
-    use iota_streams_core::prelude::{Rc, RefCell};
+    use iota_streams_core::prelude::{
+        Rc,
+        RefCell,
+    };
 
     let transport = Rc::new(RefCell::new(crate::api::tangle::BucketTransport::new()));
     assert!(dbg!(smol::block_on(example(transport)).is_ok()));
