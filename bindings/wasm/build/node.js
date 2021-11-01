05e33ca763fcb8290c605a7d9e54089707875fee
DyrellC <dyrell.chapman@iota.org>
2021-11-01 14:42:04 -0600

    Remove artefact from wasm build

--- bindings/wasm/build/node.js
+++ bindings/wasm/build/node.js
@@ -9,20 +9,8 @@ let changedFileNode = entryFileNode.replace(
     "const fetch = require(\'node-fetch\')\r\nglobal.Headers = fetch.Headers\r\nglobal.Request = fetch.Request\r\nglobal.Response = fetch.Response\r\nglobal.fetch = fetch\r\n\r\nlet imports = {};"
 )
 
-/*const entryFilePathNode2 = path.join(__dirname, '../../../../identity.rs/bindings/wasm/node/identity_wasm.js')
-const entryFileNode2 = fs.readFileSync(entryFilePathNode2).toString()
-let changedFileNode2 = entryFileNode2.replace(
-    "let imports = {};",
-    "const fetch = require(\'node-fetch\')\r\nglobal.Headers = fetch.Headers\r\nglobal.Request = fetch.Request\r\nglobal.Response = fetch.Response\r\nglobal.fetch = fetch\r\n\r\nlet imports = {};"
-)*/
-
-
 fs.writeFileSync(
     entryFilePathNode,
     changedFileNode
 )
 
-/*fs.writeFileSync(
-    entryFilePathNode2,
-    changedFileNode2
-)*/
