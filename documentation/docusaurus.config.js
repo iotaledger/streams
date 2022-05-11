52282d647ba09fb4f7b1242c7f3c698e42a2893b
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-11 22:43:00 +0000

    Cleanup PR and update cli

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -10,12 +10,12 @@ module.exports = {
       '@docusaurus/plugin-content-docs',
       {
         id: 'streams',
-        path: path.resolve(__dirname, './docs'),
+        path: path.resolve(__dirname, 'docs'),
         routeBasePath: 'streams',
-        sidebarPath: path.resolve(__dirname, './sidebars.js'),
-        editUrl: 'https://github.com/iotaledger/streams/edit/develop/',
+        sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+        editUrl: 'https://github.com/iotaledger/streams/edit/develop/documentation',
       }
     ],
   ],
-  staticDirectories: [path.resolve(__dirname, './static')],
+  staticDirectories: [path.resolve(__dirname, 'static')],
 };
