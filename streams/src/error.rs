9f6981f23180cda5cf5caf4bda2c1266b27988ff
Brord van Wierst <brord@iota.org>
2022-10-06 03:24:39 +0200

    more cleanup

--- streams/src/error.rs
+++ streams/src/error.rs
@@ -27,34 +27,11 @@ pub enum Error {
     //////////
     // Streams
     //////////
-    #[error("the user does not have an identity, but needs one to {0}")]
-    NoIdentity(&'static str),
-
-    #[error("User identity contained no secret key")]
-    NoSecretKey,
-
-    #[error("Missing {0} for {1} in order to {2}")]
-    MissingUserData(&'static str, &'static str, &'static str),
-
-    #[error("Missing role {0} for {1:?} in order to {2}")]
-    WrongRole(&'static str, Identifier, &'static str),
 
     #[error(
-        "user does not have a cursor in branch '{0}'. This probably means the user does not have write permission over that branch"
+        "address already taken for. The address '{1}' where the {0} message is being sent already contains some data, possibly spam."
     )]
-    NoCursor(Topic),
-
-    #[error("topic {0} not found in store")]
-    TopicNotFound(Topic),
-
-    #[error("Topic by hash {0} is not known")]
-    UnknownTopic(TopicHash),
-
-    #[error("PSK by id {0} is not known")]
-    UnknownPsk(PskId),
-
-    #[error("A payload must be specified in order to send a message")]
-    PayloadEmpty,
+    AddressUsed(&'static str, Address),
 
     #[error("unexpected message type {0}")]
     MessageTypeUnknown(u8),
@@ -62,8 +39,27 @@ pub enum Error {
     #[error("message '{0}' not found in {1}")]
     MessageMissing(MsgId, &'static str),
 
-    #[error("Error unwrapping the message {0}. The message at address '{1:#?}' could not be unwrapped: {2}")]
-    Unwrapping(&'static str, Address, LetsError),
+    #[error("Failed getting messages for {0} due to {1}")]
+    Messages(&'static str, anyhow::Error),
+
+    #[error("Missing {0} for {1} in order to {2}")]
+    MissingUserData(&'static str, &'static str, &'static str),
+
+    #[error(
+        "user does not have a cursor in branch '{0}'. This probably means the user does not have write permission over that branch"
+    )]
+    NoCursor(Topic),
+
+    #[error("the user does not have an identity, but needs one to {0}")]
+    NoIdentity(&'static str),
+
+    #[error("User identity contained no secret key")]
+    NoSecretKey,
+
+    #[error(
+        "not connected to a stream. A user must either create a stream or connect to an existing one before attempting to {0}"
+    )]
+    NoStream(&'static str),
 
     #[error(
         "Message not linked. The {0} message at address '{1:#?}' is not linked to any message. \
@@ -71,48 +67,44 @@ Any {0} message must be linked to a previous message by including the address of
     )]
     NotLinked(&'static str, Address),
 
-    #[error(
-        "not connected to a stream. A user must either create a stream or connect to an existing one before attempting to {0}"
-    )]
-    NoStream(&'static str),
+    #[error("A payload must be specified in order to send a message")]
+    PayloadEmpty,
 
     #[error("Setup error: {0}")]
     Setup(&'static str),
 
+    #[error("topic {0} not found in store")]
+    TopicNotFound(Topic),
+
     #[error("Transport error whilest doing {0} for address {1}: {2}")]
     Transport(Address, &'static str, LetsError),
 
-    #[error("Failed getting messages for {0} due to {1}")]
-    Messages(&'static str, anyhow::Error),
+    #[error("PSK by id {0} is not known")]
+    UnknownPsk(PskId),
+    
+    #[error("Topic by hash {0} is not known")]
+    UnknownTopic(TopicHash),
 
-    #[error(
-        "address already taken for. The address '{1}' where the {0} message is being sent already contains some data, possibly spam."
-    )]
-    AddressUsed(&'static str, Address),
+    #[error("Error unwrapping the message {0}. The message at address '{1:#?}' could not be unwrapped: {2}")]
+    Unwrapping(&'static str, Address, LetsError),
 
-    /// TODO REMOVE
-    #[error("Internal error {0} Cause: {1}")]
-    Wrapped(&'static str, lets::error::Error),
+    #[error("Missing role {0} for {1:?} in order to {2}")]
+    WrongRole(&'static str, Identifier, &'static str),
 
     #[error("Internal Spongos error: {0}")]
     Spongos(SpongosError),
 
-    #[error("Internal LETS error: {0}")]
-    Lets(LetsError),
-
     #[error("External error: {0:?}")]
     External(anyhow::Error),
+
+    /// TODO REMOVE or merge
+    #[error("Internal error {0} Cause: {1}")]
+    Wrapped(&'static str, lets::error::Error),
 }
 
 impl From<SpongosError> for Error {
     fn from(error: SpongosError) -> Self {
         Self::Spongos(error)
-    }
-}
-
-impl From<LetsError> for Error {
-    fn from(error: LetsError) -> Self {
-        Self::Lets(error)
     }
 }
 
